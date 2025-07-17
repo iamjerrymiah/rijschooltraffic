ac1524569ef3f62f73af40ed3e3baa6fbdc01288
Godwin Nwafor <nwfrgodwin@gmail.com>
2025-07-17 08:30:51 +0100

    update

---
+++ src/pages/Beekbergen/Beekbergen.tsx
@@ -0,0 +1,11 @@
+import PageMainContainer from "../../common/PageMain/PageMain";
+import Region from "../../common/Region/Region";
+
+
+export default function Beekbergen() {
+  return (
+    <PageMainContainer title="Rijschool Beekbergen" description="">
+      <Region region="Beekbergen" />
+    </PageMainContainer>
+  );
+}
